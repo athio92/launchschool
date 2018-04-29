f63052e2e6e214b952cd24a27e2dd06c86c87db0
athio92 <anthony_thio@utexas.edu>
2018-04-29 13:10:35 +0700

    mortgage_calculator.rb: after code review

--- courses/101/L02/mortgage_calculator.rb
+++ courses/101/L02/mortgage_calculator.rb
@@ -4,11 +4,10 @@ def prompt(message)
 end
 
 def integer?(input)
-  Integer(input) rescue false
-end
+  Integer(input) rescue false  #following previous lesson's suggestion
 
 def float?(input)
-  Float(input) rescue false
+  Float(input) rescue false #following previous lesson's suggestion
 end
 
 def valid_positive_number?(input)
@@ -16,7 +15,7 @@ def valid_positive_number?(input)
 end
 
 def get_float(message)
-  #Prints message and validates number that user inputted.
+  # Prints message and validates number that user inputted.
   number = ""
   loop do
     prompt(message)
@@ -29,7 +28,11 @@ def get_float(message)
     end
   end
 
-  return number
+  number
+end
+
+def calculate_monthly_payment(loan_amount, monthly_interest_rate, loan_months)
+  loan_amount * (monthly_interest_rate / (1 - (1 + monthly_interest_rate)**-loan_months))
 end
 
 # Real program starts here
@@ -37,20 +40,21 @@ prompt("Welcome to the Mortgage Calculator!")
 
 loop do
   loan_amount = get_float("Please enter your loan amount (dollars)")
-  annual_rate_decimal = get_float("Please enter your Annual Percentage Rate (in percents)")/100
+  annual_rate_decimal = get_float("Please enter your Annual Percentage Rate (in percents)") / 100
   loan_years = get_float("Please enter your loan duration (years)")
 
   prompt("Loan: #{loan_amount}, Rate:#{annual_rate_decimal}, Years:#{loan_years}")
 
   loan_months = 12 * loan_years
-  monthly_interest_rate = annual_rate_decimal/12
+  monthly_interest_rate = annual_rate_decimal / 12
 
-  monthly_payment = loan_amount * (monthly_interest_rate/(1-(1+monthly_interest_rate)**(-loan_months)))
-  prompt("Your monthly payment is: $#{format('%.2f',monthly_payment)}")
+  monthly_payment = calculate_monthly_payment(loan_amount, monthly_interest_rate, loan_months)
+  prompt("Your monthly payment is: $#{format('%.2f', monthly_payment)}")
 
   prompt("Do you want another calculation? (Y for yes)")
   answer = gets.chomp.downcase
-  break unless answer.start_with?("y")
+  break unless answer == "y"
+  print %x{clear}
 end
 
 prompt("Thank you for using this calculator. Goodbye!")
